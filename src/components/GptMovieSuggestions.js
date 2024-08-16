52984c05dc2e6337ac832a7391788ebc827c690f
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-08-16 12:01:58 +0530

    Updated

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -2,9 +2,9 @@ import MovieList from "./MovieList";
 
 const GptMovieSuggestions = ({ movies }) => {
 	return (
-		<div className="p-4 m-4 bg-black text-white bg-opacity-90">
+		<div className="p-4 m-4 bg-black text-white bg-opacity-80">
 		{movies && movies.length > 0 ? (
-		  <MovieList title="Search Results" movies={movies} />
+		  <MovieList title="Search Results" movies={movies} /> 
 		) : (
 		  <p>No results found.</p>
 		)}
