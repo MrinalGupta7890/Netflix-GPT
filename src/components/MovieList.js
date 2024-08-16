a3097bf41f27a82eaacb938c4dc5284308ed4498
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-08-16 11:13:49 +0530

    Updated

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,13 +2,12 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   // console.log(movies);
-  const filteredMovies = movies.filter(movie => movie.poster_path);
   return (
     <div className="px-4">
       <h1 className="text-xl font-semibold py-3 text-white">{title}</h1>
       <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
-          {filteredMovies.map((movie) => (
+          {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
           ))}
         </div>
