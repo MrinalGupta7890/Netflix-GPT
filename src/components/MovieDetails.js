2c41648030cc2c5e26f181c7c13e7ece8241f0ad
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-08-17 09:58:08 +0530

    Updated

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { NETFLIXLOGO } from "../utils/constant";
+import { NETFLIXLOGO, PLAY_ICON } from "../utils/constant";
 import { Link } from "react-router-dom";
 
 const MovieDetails = () => {
   return (
-    <div
-      className="fixed h-screen w-screen bg-cover"
+    <div 
+      className="fixed h-screen w-screen bg-cover "
       style={{ backgroundImage: `url('/Images/MovieDetails.png')` }}
     >
-      <div className="absolute w-screen px-16 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <div className="absolute w-full px-4 py-2 md:px-16 md:py-4 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between items-center md:items-start">
         <img
-          className="w-44 mx-auto md:mx-0"
+          className="w-32 md:w-44 mx-auto md:mx-0"
           src={NETFLIXLOGO}
           alt="Netflix Logo"
         />
         <Link to="/browse">
-          <button className="py-2 px-4 mx-8 my-4 bg-purple-500 text-white rounded-md">
+          <button className="py-2 px-6 my-4 bg-purple-500 text-white rounded-md">
             Close
           </button>
         </Link>
+      </div>
+      <div className="absolute bottom-14 left-4 md:left-20 z-10">
+      <a href="https://youtu.be/vEhYTJpC2No?si=ug37QPIQ1HONWyg_" target="_blank" rel="noopener noreferrer">
+        <button className="bg-white text-black py-2 px-8 text-xl font-bold rounded-md hover:bg-opacity-80 flex">
+          <img
+            src={PLAY_ICON}
+            alt="Play"
+            className="w-6 h-6 mr-2 inline-block align-middle"
+          />
+          Play
+        </button>
+        </a>
       </div>
     </div>
   );
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -6,26 +6,32 @@ const VideoTitle = ({ title, overview }) => {
     <div className="w-screen aspect-video pt-[18%] px-6 md:px-20 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-xl md:text-5xl font-bold">{title}</h1>
       <p className="hidden md:inline-block py-6 text-lg w-1/3">{overview}</p>
-      <Link to="/detail">
       <div className="my-2 md:m-0">
-        <button className="bg-white text-black py-1 md:py-2 px-8 text-xl font-bold rounded-md hover:bg-opacity-80">
-          <img
-            src={PLAY_ICON}
-            alt="Play"
-            className="w-6 h-6 mr-2 inline-block align-middle"
-          />
-          Play
-        </button>
-        <button className="hidden md:inline-block mx-2 bg-gray-500 text-white p-2 px-6 text-xl bg-opacity-50 rounded-md">
-        <img
-            src={MOREINFO_ICON}
-            alt="Play"
-            className="w-6 h-6 mr-2 inline-block align-middle filter invert"
-          />
-          More Info
-        </button>
+        <a
+          href="https://youtu.be/ea94nqoxnVQ?si=LWTzYiGAxQLvVD6J"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="bg-white text-black py-1 md:py-2 px-8 text-xl font-bold rounded-md hover:bg-opacity-80">
+            <img
+              src={PLAY_ICON}
+              alt="Play"
+              className="w-6 h-6 mr-2 inline-block align-middle"
+            />
+            Play
+          </button>
+        </a>
+        <Link to="/detail">
+          <button className="hidden md:inline-block mx-2 bg-gray-500 text-white p-2 px-6 text-xl bg-opacity-50 rounded-md">
+            <img
+              src={MOREINFO_ICON}
+              alt="Play"
+              className="w-6 h-6 mr-2 inline-block align-middle filter invert"
+            />
+            More Info
+          </button>
+        </Link>
       </div>
-      </Link>
     </div>
   );
 };
